f0c9345878988ef90d8eabb1becb96a2197531d0
Bladepool <100499358+CFG-NINJA@users.noreply.github.com>
2023-03-22 21:17:00 -0400

    init

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -2,8 +2,8 @@
 const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
   'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
-  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
-  'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
+  'Tres Mainnet': { rpc: 'https://rpc.tresleches.finance', tx_explorer: 'https://treschain.io/tx/' },
+  'Tres Testnet': { rpc: 'https://rpc-test.tresleches.finance', tx_explorer: 'https://treschain.co/tx/' },
 };
 
 module.exports = Network;
